d1cb2ec9ad079c0634f1b5ad4181166b525e0d95
userthalib <thalibirfan29@gmail.com>
2021-02-18 14:41:36 +0700

    Js2 Praktikum 3

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return ('Selamat Datang');
+        return ('Halaman Awal Website');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,15 @@ Route::get('/article/{id}', function($id) {
 Route::get('/', [App\Http\Controllers\PageController::class, 'index']);
 Route::get('/about', [App\Http\Controllers\PageController::class, 'about']);
 Route::get('/article/{id}', [App\Http\Controllers\PageController::class, 'article']);
-//Js2 Prak No.3
+//Js2 Prak2 No.3
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
 Route::get('/about', [App\Http\Controllers\AboutController::class, 'index']);
 Route::get('/article/{id}', [App\Http\Controllers\ArticleController::class, 'index']);
-
-
+//Js2 Prak3
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/products/{produk}', [App\Http\Controllers\ProductsController::class, 'index']);
+Route::get('/news/{tanggal}', [App\Http\Controllers\NewsController::class, 'index']);
+Route::get('/programs/{prog}', [App\Http\Controllers\ProgramController::class, 'index']);
+Route::get('/aboutUs', [App\Http\Controllers\AboutUsController::class, 'index']);
+Route::get('/contactUs', [App\Http\Controllers\ContactUsController::class, 'index']);
 
